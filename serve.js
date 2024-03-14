96df7160f9f60f099c435ee8343c6bbd7fdbdd15
JeanBariviera <jean_bariviera@hotmai.com>
2024-03-13 21:33:00 -0300

    ultimo teste

--- serve.js
+++ serve.js
@@ -1,8 +1,24 @@
 const http = require ('http');
 const server = http.createServer ((req, res) => {
+        function calcularMedia(numeros) {
+            if (numeros.length === 0) {
+                return 0;
+            }
+            let soma = 0;
+         
+            for (let i = 0; i < numeros.length; i++) {
+                soma += numeros[i];
+            }
+            const media = soma / numeros.length;
+            
+            return media;
+        }
+        const numeros = [10, 20, 30, 40, 50];
+        
+        const media = calcularMedia(numeros);
     res.statusCode = 200;
     res.setHeader ('Content-Type', 'text/plain');
-    res.end(`hello, World!\n`);
+    res.end("A média dos números é:" + media);
 });
 const port = 3000;
 const hostname = '127.0.0.1';
