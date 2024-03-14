77e2162233ea69f41bac23bc0451b636f064f1fc
JeanBariviera <jean_bariviera@hotmai.com>
2024-03-14 20:41:34 -0300

    novo

---
+++ teste/index.js
@@ -0,0 +1,12 @@
+function determinarPositivoNegativo(numero) {
+    if (numero > 0) {
+        return "O número é positivo.";
+      }
+      else if (numero < 0) {
+        return "O número é negativo.";
+      }
+      else {
+        return "O número é zero.";
+      }
+    }
+    console.log(determinarPositivoNegativo(-58));
