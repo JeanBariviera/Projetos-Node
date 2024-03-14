992de654093187d3779b32ed50914a1b7927e1e3
JeanBariviera <jean_bariviera@hotmai.com>
2024-03-13 21:29:05 -0300

    rrr

--- serve.js
+++ serve.js
@@ -2,7 +2,7 @@ const http = require ('http');
 const server = http.createServer ((req, res) => {
     res.statusCode = 200;
     res.setHeader ('Content-Type', 'text/plain');
-    res.end('hello, World!\n');
+    res.end(`hello, World!\n`);
 });
 const port = 3000;
 const hostname = '127.0.0.1';
